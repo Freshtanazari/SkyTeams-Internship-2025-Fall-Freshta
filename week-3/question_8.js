35682aad1796a758868b179a254d40d4a8dfad2c
Freshtanazari <freshtanazari0m@gmail.com>
2026-01-20 23:33:32 +0500

    solved till question 13

--- week-3/question_8.js
+++ week-3/question_8.js
@@ -1,51 +1,63 @@
 class account{    
     constructor(name, balance){
+        if(isNaN(balance) || balance < 0){
+            throw new Error("you must input a number greater than 0 ")
+        }
         this.name = name;
         this.balance = balance
     }
     deposit(val){
-
+        if(isNaN(val) || val < 0){
+            throw new Error("not a valid input")
+        }
+        this.balance += val;
+        return val + "deposited successfully";
     }
     withdraw(val){
-        
+        if(isNaN(val) || val < 0){
+            throw new Error("you must input a number greater than 0 ")
+        }
+        if (val > this.balance){
+            throw new Error("you want to withdraw more than your balance. ")
+        }
+        this.balance -= val;
+        return val + "deposited successfully";
+
     }
     displayBalance(){
         return "your account name is: "+ this.name + "\n your balance is: "+ this.balance;
     }
 }
+let newAccount = new account("Freshta", 100000);
+const prompt = require("prompt-sync")({ sigint: true });
 
 
 function ATM(){
-    userAction = prompt("please choose an option:\n a) withdraw\n b) balance\n c) deposit\n d) exit\n");
-
-    if(! userAction.includes(["a", "b", "c", "d"])){
-        return "kindly input only valid values [a, b, c, d]";
-    }
-    
-    function withdraw(val){
-        if(!Number(val)){
-            return "Only input valid numbers"
-        }
-    }
-    function deposit(val){
-        if(!Number(val)){
-            return "Only input valid numbers"
-        }
-    }
-    function displayBalance(){
         
-    }
-
-    switch(userAction){
-        case a:
-            let value = prompt("Enter the withdrawal ammount: ");
-            withdraw(value);
-        case b: 
-            displayBalance();
-        case c: 
-            deposit(val);
-        case d:
-            return
-    }
-    
+    while(true){
+        userAction = prompt("please choose an option: a) withdraw b) balance c) deposit d) exit\n");
+        if(! ["a", "b", "c", "d"].includes(userAction)){
+            console.log("kindly input only valid values [a, b, c, d]");
+        }
+        console.log(userAction);
+        let value;
+        switch(userAction){
+            case "a":
+                value = prompt("Enter the withdrawal ammount: ");
+                console.log(newAccount.withdraw(value));
+                break;
+            case "b": 
+                console.log(newAccount.displayBalance());
+                break;
+            case "c": 
+                value = prompt("Enter the deposit ammount: ");
+                console.log(newAccount.deposit(value));
+                break;
+            case "d":
+                console.log("You have successfully exitted the program")
+                return
+        }
+    } 
 }
+
+ATM();
